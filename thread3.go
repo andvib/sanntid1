c937bb960b55bbc8d1e67db151d724e2fb8f9eaf
andvib <andvibeto@gmail.com>
2015-01-22 11:31:02 +0100

    Dag 2

--- thread3.go
+++ thread3.go
@@ -6,20 +6,31 @@ import (."fmt"
 
 var i int
 
+var c = make(chan int,1)
+
 func threadOne(){
 	for x := 0 ; x <= 1000000 ; x++{
+		for; <-c != 1; {}
+		c<-0
 		i++
+		c<-1
 	}
 }
 
 func threadTwo(){
 	for y := 0 ; y <= 1000000 ; y++{
+		for ;<-c != 1;{}
+		c<-0
 		i--
+		c<-1
 	}
 }
 
 func main() {
 	i = 0
+	
+	
+
 	runtime.GOMAXPROCS(runtime.NumCPU())
 
 	go threadOne()
